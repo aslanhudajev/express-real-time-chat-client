505b79218655dc4e93d3b1dd49a4f7a2c012474b
Aslan <aslambek.hudajev@gmail.com>
2024-07-24 23:10:59 +0200

    first commit, a little late, but it is what it is.

---
+++ src/components/parts/NavigationHeader.jsx
@@ -0,0 +1,88 @@
+/* eslint-disable react/prop-types */
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
+
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav className="w-full p-4 shadow-sm sticky top-0 z-50 bg-white">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <h2 className="logo scroll-m-20 text-3xl font-semibold tracking-tight mr-4">
+              Marble
+            </h2>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to="/" className={navigationMenuTriggerStyle()}>
+              Blog
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Socials</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[350px] gap-3 p-4">
+                <ListItem
+                  to="https://github.com/aslanhudajev?tab=repositories"
+                  title="Github"
+                >
+                  My Github account
+                </ListItem>
+                <ListItem
+                  to="https://github.com/aslanhudajev?tab=repositories"
+                  title="LinkedIn"
+                >
+                  My LinkedIn profile
+                </ListItem>
+                <ListItem
+                  to="https://github.com/aslanhudajev?tab=repositories"
+                  title="LeetCode"
+                >
+                  My LeetCode solutions
+                </ListItem>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to="/aboutme" className={navigationMenuTriggerStyle()}>
+              About
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </nav>
+  );
+};
+
+export default Header;
+
+const ListItem = ({ to, title, children, className, ...props }) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          href={to}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+};
---
+++ src/socket.js
@@ -0,0 +1,3 @@
+import { io } from "socket.io-client";
+const URL = "http://localhost:3000";
+export default io(URL);
